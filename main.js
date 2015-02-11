e60a090f73edffb21ef458a008e07ffc0039e86a
James Duran <jmduran@broncs.utpa.edu>
2015-02-11 23:14:47 +0000

    Added initial skeleton code for three.js scene.

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+Physijs.scripts.worker = "/physijs/physijs_worker.js";
+Physijs.scripts.ammo = "/ammojs/ammo.js";
+
+var initScene, render, renderer, scene, camera, box;
+
+initScene = function() {
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById("viewport").appendChild(renderer.domElement);
+
+    scene = new Physijs.Scene;
+
+    camera = new THREE.PerspectiveCamera(
+        35,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+
+    camera.position.set(60, 50, 60);
+    camera.lookAt(scene.position);
+    scene.add(camera);
+
+    box = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(5, 5, 5),
+        new THREE.MeshBasicMaterial({color: 0x888888})
+    );
+    scene.add(box);
+
+    requestAnimationFrame(render);
+};
+
+render = function() {
+    scene.simulate();
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+};
+
+window.onload = initScene();
