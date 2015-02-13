816dc9951b08ecc374503e9d724feecec6bd269f
James Duran <jmduran@broncs.utpa.edu>
2015-02-13 15:06:12 +0000

    Implemented initial blocks and pshapes arrays, as well as initial logic for the reset world button.

--- main.js
+++ main.js
@@ -3,9 +3,30 @@
 Physijs.scripts.worker = "/physijs/physijs_worker.js";
 Physijs.scripts.ammo = "/ammojs/ammo.js";
 
-var initScene, render, renderer, scene, camera, controls, box, ground, light, resize;
+var renderer, scene, camera, controls;
 
-initScene = function() {
+var blocks = [
+    {
+        pos: {x: 0, y: 10, z: 0},
+        rot: {
+            x: Math.random() * Math.PI,
+            y: Math.random() * Math.PI,
+            z: Math.random() * Math.PI
+        }
+    },
+    {
+        pos: {x: 0, y: 20, z: 0},
+        rot: {
+            x: Math.random() * Math.PI,
+            y: Math.random() * Math.PI,
+            z: Math.random() * Math.PI
+        }
+    }
+];
+
+var pshapes = [];
+
+var initScene = function() {
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor( 0xffffff, 1);
@@ -27,7 +48,7 @@ initScene = function() {
     camera.lookAt(scene.position);
     scene.add(camera);
 
-    light = new THREE.DirectionalLight(0xFFFFFF);
+    var light = new THREE.DirectionalLight(0xFFFFFF);
     light.position.set(20, 40, -15);
     light.target.position.copy(scene.position);
     light.castShadow = true;
@@ -44,7 +65,7 @@ initScene = function() {
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    ground = new Physijs.BoxMesh(
+    var ground = new Physijs.BoxMesh(
         new THREE.CubeGeometry(50, 1, 50),
         new THREE.MeshPhongMaterial({overdraw: true, color: 0x888888}),
         0
@@ -52,30 +73,37 @@ initScene = function() {
     ground.receiveShadow = true;
     scene.add(ground);
 
-    box = new Physijs.BoxMesh(
-        new THREE.CubeGeometry(5, 5, 5),
-        new THREE.MeshPhongMaterial({overdraw: true, color: 0xFFF})
-    );
-    box.position.y = 10;
-    box.rotation.set(
-        Math.random() * Math.PI,
-        Math.random() * Math.PI,
-        Math.random() * Math.PI
-    );
-    box.castShadow = true;
-    box.receiveShadow = true;
-    scene.add(box);
+    blocks.forEach(createShape);
 
     requestAnimationFrame(render);
 };
 
-render = function() {
+var createShape = function(e, i, a) {
+    var box = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(5, 5, 5),
+        new THREE.MeshPhongMaterial({overdraw: true, color: 0xFFF})
+    );
+
+    box.position.set(e.pos.x, e.pos.y, e.pos.z);
+    box.rotation.set(e.rot.x, e.rot.y, e.rot.z);
+    box.castShadow = true;
+    box.receiveShadow = true;
+
+    pshapes[i] = box;
+    scene.add(pshapes[i]);
+};
+
+var removeFromScene = function(e, i, a) {
+    scene.remove(e);
+};
+
+var render = function() {
     scene.simulate();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 };
 
-resize = function() {
+var resize = function() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -83,8 +111,16 @@ resize = function() {
     controls.update();
 };
 
-$("#toggle-sidebar").click(function(){
+$("#run-button").click(function() {
     $("#application-sidebar").sidebar("toggle");
+});
+
+$("#sidebar-button").click(function(){
+    $("#application-sidebar").sidebar("toggle");
+});
+
+$("#reset-button").click(function(){
+    pshapes.forEach(removeFromScene);
 });
 
 window.onload = initScene();
