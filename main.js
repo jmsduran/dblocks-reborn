cbfb60676adbe74517e007ca7152b05899c10cb4
James Duran <jmduran@broncs.utpa.edu>
2015-02-13 23:28:51 +0000

    Refactored JS code.

--- main.js
+++ main.js
@@ -1,120 +1,133 @@
 'use strict';
 
-Physijs.scripts.worker = "/physijs/physijs_worker.js";
-Physijs.scripts.ammo = "/ammojs/ammo.js";
+var DBLOCKS = (function() {
+    Physijs.scripts.worker = "/physijs/physijs_worker.js";
+    Physijs.scripts.ammo = "/ammojs/ammo.js";
 
-var renderer, scene, camera, controls;
+    var renderer, scene, camera, controls;
 
-var blocks = [];
-var pshapes = [];
+    var createShape = function(e, i, a) {
+        var box = new Physijs.BoxMesh(
+            new THREE.CubeGeometry(3, 7, 1),
+            new THREE.MeshPhongMaterial({
+                overdraw: true,
+                color: 0xF7BE81
+            })
+        );
 
-var initScene = function() {
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xFFFFFF, 1);
-    renderer.shadowMapEnabled = true;
-    renderer.shadowMapSoft = true;
-    document.getElementById("viewport").appendChild(renderer.domElement);
+        box.position.set(e.pos.x, e.pos.y, e.pos.z);
+        box.rotation.set(e.rot.x, e.rot.y, e.rot.z);
+        box.castShadow = true;
+        box.receiveShadow = true;
 
-    scene = new Physijs.Scene;
-    scene.setGravity(new THREE.Vector3(0, -30, 0));
+        DBLOCKS.pshapes[i] = box;
+        scene.add(DBLOCKS.pshapes[i]);
+    };
 
-    camera = new THREE.PerspectiveCamera(
-        35,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000
-    );
+    var removeFromScene = function(e, i, a) {
+        scene.remove(e);
+    };
 
-    camera.position.set(60, 50, 60);
-    camera.lookAt(scene.position);
-    scene.add(camera);
+    var render = function() {
+        scene.simulate();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    };
 
-    var light = new THREE.DirectionalLight(0xFFFFFF);
-    light.position.set(20, 40, -15);
-    light.target.position.copy(scene.position);
-    light.castShadow = true;
-    light.shadowCameraLeft = -60;
-    light.shadowCameraTop = -60;
-    light.shadowCameraRight = 60;
-    light.shadowCameraBottom = 60;
-    light.shadowCameraNear = 20;
-    light.shadowCameraFar = 200;
-    light.shadowBias = -.0001;
-    light.shadowMapWidth = light.shadowMapHeight = 2048;
-    light.shadowDarkness = .7;
-    scene.add(light);
+    return {
+        blocks: [],
 
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
+        pshapes: [],
 
-    var ground = new Physijs.BoxMesh(
-        new THREE.CubeGeometry(100, 1, 100),
-        new THREE.MeshPhongMaterial({overdraw: true, color: 0x888888}),
-        0
-    );
-    ground.receiveShadow = true;
-    scene.add(ground);
+        start: function() {
+            renderer = new THREE.WebGLRenderer({
+                antialias: true
+            });
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setClearColor(0xFFFFFF, 1);
+            renderer.shadowMapEnabled = true;
+            renderer.shadowMapSoft = true;
+            document.getElementById("viewport").appendChild(renderer.domElement);
 
-    if ($("#application-editor").val())
-        runCode();
+            scene = new Physijs.Scene;
+            scene.setGravity(new THREE.Vector3(0, -30, 0));
 
-    requestAnimationFrame(render);
-};
+            camera = new THREE.PerspectiveCamera(
+                35,
+                window.innerWidth / window.innerHeight,
+                1,
+                1000
+            );
 
-var createShape = function(e, i, a) {
-    var box = new Physijs.BoxMesh(
-        new THREE.CubeGeometry(3, 7, 1),
-        new THREE.MeshPhongMaterial({overdraw: true, color: 0xF7BE81})
-    );
+            camera.position.set(60, 50, 60);
+            camera.lookAt(scene.position);
+            scene.add(camera);
 
-    box.position.set(e.pos.x, e.pos.y, e.pos.z);
-    box.rotation.set(e.rot.x, e.rot.y, e.rot.z);
-    box.castShadow = true;
-    box.receiveShadow = true;
+            var light = new THREE.DirectionalLight(0xFFFFFF);
+            light.position.set(20, 40, -15);
+            light.target.position.copy(scene.position);
+            light.castShadow = true;
+            light.shadowCameraLeft = -60;
+            light.shadowCameraTop = -60;
+            light.shadowCameraRight = 60;
+            light.shadowCameraBottom = 60;
+            light.shadowCameraNear = 20;
+            light.shadowCameraFar = 200;
+            light.shadowBias = -.0001;
+            light.shadowMapWidth = light.shadowMapHeight = 2048;
+            light.shadowDarkness = .7;
+            scene.add(light);
 
-    pshapes[i] = box;
-    scene.add(pshapes[i]);
-};
+            controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-var removeFromScene = function(e, i, a) {
-    scene.remove(e);
-};
+            var ground = new Physijs.BoxMesh(
+                new THREE.CubeGeometry(100, 1, 100),
+                new THREE.MeshPhongMaterial({
+                    overdraw: true,
+                    color: 0x888888
+                }),
+                0
+            );
+            ground.receiveShadow = true;
+            scene.add(ground);
 
-var resetWorld = function() {
-    pshapes.forEach(removeFromScene);
-    pshapes = [];
-    blocks = [];
-};
+            if ($("#application-editor").val())
+                DBLOCKS.runCodeHandler();
 
-var toggleSidebar = function() {
-    $("#application-sidebar").sidebar("toggle");
-};
+            requestAnimationFrame(render);
+        },
 
-var render = function() {
-    scene.simulate();
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
-};
+        resizeHandler: function() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
 
-var resize = function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            controls.update();
+        },
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    controls.update();
-};
+        resetWorldHandler: function() {
+            DBLOCKS.pshapes.forEach(removeFromScene);
+            DBLOCKS.pshapes = [];
+            DBLOCKS.blocks = [];
+        },
 
-var runCode = function() {
-    toggleSidebar();
-    resetWorld();
+        toggleSidebarHandler: function() {
+            $("#application-sidebar").sidebar("toggle");
+        },
 
-    eval($("#application-editor").val());
-    blocks.forEach(createShape);
-};
+        runCodeHandler: function() {
+            DBLOCKS.toggleSidebarHandler();
+            DBLOCKS.resetWorldHandler();
 
-$("#run-button").click(runCode);
-$("#sidebar-button").click(toggleSidebar);
-$("#reset-button").click(resetWorld);
+            eval($("#application-editor").val());
+            DBLOCKS.blocks.forEach(createShape);
+        }
+    };
+})();
 
-window.onload = initScene();
-window.addEventListener("resize", resize, false);
+$("#run-button").click(DBLOCKS.runCodeHandler);
+$("#sidebar-button").click(DBLOCKS.toggleSidebarHandler);
+$("#reset-button").click(DBLOCKS.resetWorldHandler);
+
+window.onload = DBLOCKS.start();
+window.addEventListener("resize", DBLOCKS.resizeHandler, false);
